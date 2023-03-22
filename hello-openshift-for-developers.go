4194c7189ef2e4421b18429aa6dd83a604f86875
Matti Luukkainen <mluukkai@iki.fi>
2023-03-22 14:12:15 +0200

    Update hello-openshift-for-developers.go

--- hello-openshift-for-developers.go
+++ hello-openshift-for-developers.go
@@ -9,7 +9,7 @@ import (
 func helloHandler(w http.ResponseWriter, r *http.Request) {
 	response := os.Getenv("RESPONSE")
 	if len(response) == 0 {
-		response = "Modified hello OpenShift for Developers!"
+		response = "WTF"
 	}
 
 	fmt.Fprintln(w, response)
